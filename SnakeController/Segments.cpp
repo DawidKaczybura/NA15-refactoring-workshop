a3b543f12477ef58423ecaf5fb118ff0577a307a
Dawid Kaczybura <kaczybura.dawid@gmail.com>
2021-09-03 00:21:12 +0200

    clean up

--- SnakeController/Segments.cpp
+++
@@ -1,8 +0,0 @@
-#include "Segments.hpp"
-#include <algorithm>
-
-bool Segments::isSegmentAtPosition(int x, int y) const
-{
-    return m_segments.end() !=  std::find_if(m_segments.cbegin(), m_segments.cend(),
-        [x, y](auto const& segment){ return segment.x == x and segment.y == y; });
-}
